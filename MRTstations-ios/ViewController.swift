8e30c71f269ab536152e1898b5cabe4eb575ae50
travis Huang <travis965@gmail.com>
2016-05-09 13:58:50 +0800

    first-init

---
+++ MRTstations-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRTstations-ios
+//
+//  Created by huang tsun yu on 2016/5/8.
+//  Copyright © 2016年 huang tsun yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '9.0'
+# Uncomment this line if you're using Swift
+ use_frameworks!
+
+target 'MRTstations-ios' do
+  pod 'SwiftyJSON', '~> 2.3'
+end
+
